34b40dc7500ad3c46a38f5571c14c85b15e2db2c
YtalloLima <ytalloaugusto@gmail.com>
2019-11-01 17:08:51 -0300

    Alterações no index... mostrar e editar prontos

--- cartolinha/app/controllers/rounds_controller.rb
+++ cartolinha/app/controllers/rounds_controller.rb
@@ -1,5 +1,16 @@
 class RoundsController < ApplicationController
 	def index
-		@rounds = Round.order :nome
+		@rounds = Round.order :id
+	end
+
+	def show
+		@round = Round.find(params[:id])
+	end
+
+	def destroy
+		@round = Round.find(params[:id])
+		@round.destroy
+
+		redirect_to(action: "index")
 	end
 end
