a6f42731353225609ab0017077c610643a010750
YtalloLima <ytalloaugusto@gmail.com>
2019-11-04 08:49:48 -0300

    Novo e Edição criados

--- cartolinha/app/controllers/rounds_controller.rb
+++ cartolinha/app/controllers/rounds_controller.rb
@@ -13,4 +13,30 @@ class RoundsController < ApplicationController
 
 		redirect_to(action: "index")
 	end
+
+	def new
+		@round = Round.new
+	end
+
+	def create
+		#nome = params[:nome]
+		@round = Round.new(round_params)
+		@round.save
+		redirect_to(action: "show", id: @round)
+	end
+
+	def edit
+		@round = Round.find(params[:id])
+	end
+
+	def update
+		@round = Round.find(params[:id])
+		@round.update_attributes(round_params)
+
+		redirect_to(action: "show", id: @round)
+	end	
+
+	def round_params
+		params.require(:round).permit(:nome)
+	end
 end
